37acf0a7d1efea691c8ee428ab2356afa965f7d2
Vinicius Dias <carlosv775@gmail.com>
2019-04-18 20:30:50 -0300

    New tests for RestulList created

--- tests/Unit/ResultListTest.php
+++ tests/Unit/ResultListTest.php
@@ -9,7 +9,7 @@ class ResultListTest extends TestCase
 {
     public function testAddResult()
     {
-        $result = new Result();
+        $result = $this->getResultMock();
         $list = new ResultList();
         $list->addResult($result);
 
@@ -24,9 +24,36 @@ class ResultListTest extends TestCase
     public function testGetResultsMustReturnUnmodifiableList(ResultList $list)
     {
         $results = $list->getResults();
-        $results->push(new Result());
+        $results->push($this->getResultMock());
 
         static::assertCount(1, $list->getResults());
         static::assertCount(2, $results);
+    }
+
+    public function testInstantiateResultListWithOrWithoutConstructorParameterMustWork()
+    {
+        $resultListWithoutCapacity = new ResultList();
+        $resultListWithCapacity = new ResultList(1);
+
+        static::assertInstanceOf(ResultList::class, $resultListWithoutCapacity);
+        static::assertInstanceOf(ResultList::class, $resultListWithCapacity);
+    }
+
+    public function testGetIteratorMustReturnAllItems()
+    {
+        $result1 = $this->getResultMock();
+        $result2 = $this->getResultMock();
+        $resultList = new ResultList(2);
+
+        $resultList->addResult($result1);
+        $resultList->addResult($result2);
+
+        static::assertEquals([$result1, $result2], iterator_to_array($resultList->getIterator()));
+    }
+
+    private function getResultMock(): Result
+    {
+        return $this->getMockBuilder(Result::class)
+            ->getMock();
     }
 }
